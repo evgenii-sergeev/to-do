3df6dc823a9b8a161a152ef77432b710d7815d51
Evgenii Sergeev <56795482+evgenii-sergeev@users.noreply.github.com>
2024-01-04 09:25:00 +0000

    add modal plugin

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Modals from "./plugins/modals"
+
+createApp(App).use(Modals).mount('#app')
